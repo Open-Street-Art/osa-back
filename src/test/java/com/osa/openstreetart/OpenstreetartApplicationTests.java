e7a36dfc2e0e4024dc91d6985ed9b7db4caefb94
Arnaud Peralta <arnaud.peralta@gmail.com>
2021-02-01 11:10:05 +0100

    fix: code smell

--- src/test/java/com/osa/openstreetart/OpenstreetartApplicationTests.java
+++ src/test/java/com/osa/openstreetart/OpenstreetartApplicationTests.java
@@ -1,10 +1,10 @@
 package com.osa.openstreetart;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.test.web.servlet.MockMvc;
 
 // Pour les futurs tests unitaires
 // import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -14,12 +14,12 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 class OpenstreetartApplicationTests {
 
-	@Autowired
-	MockMvc mvc;
+	// @Autowired
+	// MockMvc mvc;
 
-	@Test
-	void testCI() throws Exception {
-		return;
-	}
+	// @Test
+	// void testCI() throws Exception {
+	// 	return;
+	// }
 
 }
