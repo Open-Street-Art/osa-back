d484f955cbf65078419f32e2aa2b5e725cb4fdec
Arnaud Peralta <arnaud.peralta@gmail.com>
2021-02-01 09:38:46 +0100

    First commit

---
+++ src/main/java/com/osa/openstreetart/OpenstreetartApplication.java
@@ -0,0 +1,13 @@
+package com.osa.openstreetart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenstreetartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenstreetartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osa/openstreetart/OpenstreetartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osa.openstreetart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenstreetartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
