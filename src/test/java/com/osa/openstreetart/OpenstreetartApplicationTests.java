d9e666abd446b36e9c5229c5fc32d592595e3157
Arnaud Peralta <arnaud.peralta@gmail.com>
2021-02-01 11:04:27 +0100

    feat: testing test on action

--- src/test/java/com/osa/openstreetart/OpenstreetartApplicationTests.java
+++ src/test/java/com/osa/openstreetart/OpenstreetartApplicationTests.java
@@ -1,13 +1,25 @@
 package com.osa.openstreetart;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+// Pour les futurs tests unitaires
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class OpenstreetartApplicationTests {
 
+	@Autowired
+	MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	void testCI() throws Exception {
+		return;
 	}
 
 }
