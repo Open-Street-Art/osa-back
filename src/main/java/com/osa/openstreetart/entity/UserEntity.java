856ea93a9f109e921ae1245d58edd96a46757725
Arnaud Peralta <arnaud.peralta@gmail.com>
2021-02-06 11:15:09 +0100

    feat: JPA installed

--- src/main/java/com/osa/openstreetart/entity/UserEntity.java
+++ src/main/java/com/osa/openstreetart/entity/UserEntity.java
@@ -1,20 +1,42 @@
 package com.osa.openstreetart.entity;
 
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class UserEntity {
 
-    private Integer id;
-    private String email;
-    private String username;
-    private String password;
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String username;
+	
+	@Column(nullable = false)
+	private String password;
+	
 	private String description;
+
 	private byte[] profilePicture;
+
 	private boolean isPublic;
-	private List<RoleEnum> roles;
-	private List<UserEntity> favArtists;
+
+	// TODO : Relation OneToMany sur l'enum
+	// private List<RoleEnum> roles;
+
+	// TODO : Relation OnetoMany
+	// private List<UserEntity> favArtists;
 	
     // TODO implémentation des classes  CityEntity, ArtEntity
     //List<CityEntity> favCities
