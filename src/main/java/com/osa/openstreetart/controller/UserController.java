c05f79fd915cbcfc084bc496199889a209ba26c2
Arnaud Peralta <arnaud.peralta@gmail.com>
2021-02-09 14:16:51 +0100

    feat: patchUserEmail unit test

--- src/main/java/com/osa/openstreetart/controller/UserController.java
+++ src/main/java/com/osa/openstreetart/controller/UserController.java
@@ -3,18 +3,18 @@ package com.osa.openstreetart.controller;
 import java.util.Optional;
 
 import com.osa.openstreetart.entity.UserEntity;
-import com.osa.openstreetart.exceptions.OSA400Exception;
-import com.osa.openstreetart.exceptions.OSA401Exception;
 import com.osa.openstreetart.repository.UserRepository;
 import com.osa.openstreetart.service.UserService;
 import com.osa.openstreetart.util.ApiRestController;
 import com.osa.openstreetart.util.JwtUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.server.ResponseStatusException;
 
 @ApiRestController
 public class UserController {
@@ -30,13 +30,14 @@ public class UserController {
 
 	@PatchMapping(value = "/user/email")
 	public ResponseEntity<String> patchUserEmail(@RequestHeader(value="Authorization") String token, @RequestBody String newMail) throws Exception {
-		String email = jwtUtil.getUsernameFromToken(token);
-		Optional<UserEntity> optionalUser = userRepo.findByUsername(email);
+		String email = jwtUtil.getUsernameFromToken(token.substring(7));
+		Optional<UserEntity> optionalUser = userRepo.findByEmail(email);
+		
 		if (!optionalUser.isPresent())
-			throw new OSA401Exception("No user found.");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No user found.");
 
 		if (!userService.isValidEmailAddress(newMail))
-			throw new OSA400Exception("Invalid email address.");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid email address.");
 
 		optionalUser.get().setEmail(newMail);
 		userRepo.save(optionalUser.get());
