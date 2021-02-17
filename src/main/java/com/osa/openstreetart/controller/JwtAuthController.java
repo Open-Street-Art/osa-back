3fc85781e4a5d719505a6c64dd0ff72224f0cb26
Arnaud Peralta <arnaud.peralta@gmail.com>
2021-02-17 15:22:35 +0100

    fix: swagger + jpa hibernate update + jwtfilter

--- src/main/java/com/osa/openstreetart/controller/JwtAuthController.java
+++ src/main/java/com/osa/openstreetart/controller/JwtAuthController.java
@@ -9,12 +9,11 @@ import com.osa.openstreetart.service.UserService;
 import com.osa.openstreetart.util.ApiRestController;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -25,12 +24,12 @@ public class JwtAuthController {
 	@Autowired
 	private UserService userService;
 
-	@ApiOperation( value = "Création d'un nouveau compte osa", 
-		notes = "Api assurant la création d’un utilisateur au sein de la base de données.")
+	@ApiOperation(value = "Création d'un nouveau compte OpenStreetArts.", 
+		notes = "API assurant la création d’un utilisateur au sein de la base de données.")
 	@ApiResponses(value = {
-		@ApiResponse(code = 201, message = "Nouveau compte crée avec succès"),
-		@ApiResponse(code = 400, message = "Echec de création du nouveau compte"),
-		@ApiResponse(code = 409, message = "Le nom d'utilisateur est déjà pris")
+		@ApiResponse(code = 201, message = "Nouveau compte crée avec succès."),
+		@ApiResponse(code = 400, message = "Echec de la création du nouveau compte."),
+		@ApiResponse(code = 409, message = "Le nom d'utilisateur est déjà pris.")
 	})
 	@PostMapping(value = "/register")
 	public ResponseEntity<String> postRegister(@RequestBody UserRegisterDTO user)
@@ -40,13 +39,14 @@ public class JwtAuthController {
 	}
 
 	
-	@ApiOperation(value = "Authentification d'un utilisateur", 
-		notes = "Api assurant l’authentification d’un utilisateur en lui renvoyant un token JWT valide 5 jours utilisable sur les routes de l’API",
+	@ApiOperation(value = "Authentification d'un utilisateur.", 
+		notes = "API assurant l’authentification d’un utilisateur en lui renvoyant un token JWT valide 5 jours utilisable sur les routes de l’API.",
 		response = JwtDTO.class)
 	@ApiResponses(value = {
-		@ApiResponse(code = 200, message = "Authentification réussi"),
-		@ApiResponse(code = 400, message = "Echec d'authentification")
+		@ApiResponse(code = 200, message = "Authentification réussi."),
+		@ApiResponse(code = 400, message = "Echec de l'authentification.")
 	})
+	@ApiImplicitParam(name = "Authorization", value = "Access Token", required = true, allowEmptyValue = false, paramType = "header", example = "Bearer access_token")
 	@PostMapping(value = "/authenticate")
 	public ResponseEntity<JwtDTO> postAuthenticate(@RequestBody UserLoginDTO request) {
 		final String token = userService.login(request);
