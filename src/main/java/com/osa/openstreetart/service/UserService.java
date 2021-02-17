04ed05ef525e8c41bf25f8ce4f3c8d8f8ff81618
Arnaud Peralta <arnaud.peralta@gmail.com>
2021-02-17 15:45:54 +0100

    fix: sonar bugs

--- src/main/java/com/osa/openstreetart/service/UserService.java
+++ src/main/java/com/osa/openstreetart/service/UserService.java
@@ -71,7 +71,7 @@ public class UserService {
 	}
 
 	public boolean isValidEmailAddress(String email) {
-		String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+		String ePattern = "^(.+)@(.+)$";
 		java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
 		java.util.regex.Matcher m = p.matcher(email);
 		return m.matches();
