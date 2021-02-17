a3ff514597c67a5f20a94c33d72790776f2e5dd0
Arnaud Peralta <arnaud.peralta@gmail.com>
2021-02-17 15:45:54 +0100

    fix: sonar bugs

--- src/main/java/com/osa/openstreetart/util/JwtUtil.java
+++ src/main/java/com/osa/openstreetart/util/JwtUtil.java
@@ -19,7 +19,7 @@ public class JwtUtil implements Serializable {
 
 	private static final long serialVersionUID = -2550185165626007488L;
 
-	public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+	public static final int JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
 	public static final String AUTHORITIES_KEY = "roles";
 
