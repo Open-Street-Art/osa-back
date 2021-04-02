03e0dbe0affe74318dbcc3f01fbbff1eb5212197
Arnaud Peralta <arnaud.peralta@gmail.com>
2021-04-02 08:02:38 +0200

    feat: bidirectionnal relation art, city

--- src/main/java/com/osa/openstreetart/entity/CityEntity.java
+++ src/main/java/com/osa/openstreetart/entity/CityEntity.java
@@ -1,8 +1,15 @@
 package com.osa.openstreetart.entity;
 
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import javax.persistence.GeneratedValue;
 
 import lombok.Data;
@@ -17,4 +24,8 @@ public class CityEntity {
 
     @Column(nullable = false)
     private String name;
+
+	@OneToMany(mappedBy="city")
+	@JsonIdentityInfo(generator= ObjectIdGenerators.UUIDGenerator.class, property="uuid")
+	private Collection<ArtEntity> arts;
 }
