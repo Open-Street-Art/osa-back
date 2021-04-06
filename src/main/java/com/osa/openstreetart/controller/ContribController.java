315ab78f5abae0c77a66a16d375d2a67e5aef1fc
abdellah <saley.abdellah@gmail.com>
2021-04-06 11:31:21 +0200

    feat: getUserContribs + unit test

--- src/main/java/com/osa/openstreetart/controller/ContribController.java
+++ src/main/java/com/osa/openstreetart/controller/ContribController.java
@@ -183,4 +183,22 @@ public class ContribController {
 			)
 		);
 	}
+
+	@GetMapping(value = "/contrib/user/contribs")
+	public ResponseEntity<OSAResponseDTO> getUserContribs(@RequestHeader(value = "Authorization") String token) throws OSA400Exception {
+
+		String username = jwtUtil.getUsernameFromToken(token.substring(tokenPrefix.length()));
+		Optional<UserEntity> user = userRepo.findByUsername(username);
+		if (!user.isPresent())
+			throw new OSA400Exception(userNotFoundMsg);
+
+			List<ContribEntity> contribs = new ArrayList<>();
+			//récupérer les contributions de l'utilisateur
+			for (ContribEntity contrib : contribRepo.findAll()) {
+				if (contrib.getContributor().equals(user.get()))
+					contribs.add(contrib);
+			}
+	
+			return ResponseEntity.ok(new OSAResponseDTO(contribTransf.modelsToDtos(contribs)));
+	}
 }
