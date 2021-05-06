59367e9aa826c5d8aa2dd4aa4a60f47e5238bf31
Arnaud Peralta <arnaud.peralta@gmail.com>
2021-05-06 16:06:01 +0200

    fix: arts samples in Rouen

--- src/main/resources/db/migration/V1.1__sample-data.sql
+++ src/main/resources/db/migration/V1.1__sample-data.sql
@@ -1,5 +1,5 @@
 insert into user_entity (id, email, username, password, is_public) values (1, 'root@mail.com', 'root', '$2a$10$0A.WWMMxy3mCHi7gZbtc3O1HHWRuzY2kGd.vrxi1r9OlgUxtoTa1O', true);
-insert into city_entity (id, name) values (1, 'Saint-Étienne-du-Rouvray');
+insert into city_entity (id, name) values (1, 'Rouen');
 insert into user_roles (user_id, role) values (1, 'ROLE_ADMIN');
 
 
